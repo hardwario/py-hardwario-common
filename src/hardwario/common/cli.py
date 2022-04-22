6a6ff9c4c403a876e76f441426f091813b9204c6
Karel Blavka <karel.blavka@hardwario.com>
2022-04-22 14:19:48 +0200

    Change logging to loguru

--- src/hardwario/common/cli.py
+++ src/hardwario/common/cli.py
@@ -1,32 +1,43 @@
 import os
 import sys
-import logging
 import pkgutil
 import importlib
 import click
 import hardwario
+from loguru import logger
+
+DEFAULT_LOG_LEVEL = 'DEBUG'
+DEFAULT_LOG_FILE = os.path.expanduser("~/.hardwario/console.log")
+
+os.makedirs(os.path.expanduser("~/.hardwario"), exist_ok=True)
 
 
 @click.group()
 @click.option('--log', 'log_level', type=click.Choice(['debug', 'info', 'warning', 'error']), help='Log level', default="warning")
 def cli_root(log_level):
     '''HARDWARIO Command Line Tool.'''
-    logging.getLogger().setLevel(log_level.upper())
+    logger.add(sys.stderr, level=log_level.upper())
 
 
 def main():
     '''Application entry point.'''
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)s: %(name)s: %(message)s')
+
+    logger.remove()
+    logger.add(DEFAULT_LOG_FILE,
+               format="{time} | {level} | {name}.{function}: {message}", level="TRACE", rotation="10 MB")
+
+    logger.debug('Argv: {}', sys.argv)
 
     # discovered and load hardwario cli plugins
     for finder, name, ispkg in pkgutil.iter_modules(hardwario.__path__, hardwario.__name__ + '.'):
         if not ispkg or name == 'hardwario.common':
             continue
+        logger.debug('Discovered module: {}', name)
         try:
             module = importlib.import_module(name + '.cli')
             cli_root.add_command(module.cli)
         except Exception as e:
+            logger.warning('Module cli import: {}', e)
             if os.getenv('DEBUG', False):
                 raise e
             pass
